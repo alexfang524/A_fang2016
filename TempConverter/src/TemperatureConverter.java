f079671b4f8924a85ef6e9138fd9ef61f8684a85
alexfang524 <18fanga73@millshighsch.org>
2016-09-19 08:16:04 -0700

    Edit to tempcovnerter

--- TempConverter/src/TemperatureConverter.java
+++ TempConverter/src/TemperatureConverter.java
@@ -2,6 +2,11 @@
 /*If its given -32*F it outputs 0*C. Use variables to represent both temperatures. 
  * Include comments that contain your name,
  * the program name and the date. Enter and compile your code in Eclipse. (5/9)(F-32) = C
+ * 
+ * Alex Fang
+ * 9/1/16
+ * AP Comp Sci First period
+ * This program would convert Celsius to Farenheit.
  */
 
 
