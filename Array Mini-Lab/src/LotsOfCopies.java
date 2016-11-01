cf95d9ff2125b7e3077387c6304592e6a2dc3710
alexfang524 <18fanga73@millshighsch.org>
2016-11-01 09:23:41 -0700

    post haloween

--- Array Mini-Lab/src/LotsOfCopies.java
+++ Array Mini-Lab/src/LotsOfCopies.java
@@ -1,24 +1,26 @@
 import java.util.Arrays;
-//alex fang first period
+//Alex Fang
+//first period
+//10/31/16
 
 public class LotsOfCopies {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	public static void main (String [] args){
+		
 		int num = 7;
 		String strMain = "APCS";
-		int[] arrMain = {1, 2, 3, 4, 5};
+		int [] arrMain = {1,2,3,4,5};
 		changeMe(num, strMain, arrMain);
 		System.out.println(num);
 		System.out.println(strMain);
 		System.out.println(Arrays.toString(arrMain));
-		
 	}
 	
-	public static void changeMe(int x, String str, int[] arr){
-		x = 8;
-		str = "I like poop";
-		arr  = {2, 4, 6, 8 ,10};
+	public static void changeMe(int x, String str, int [] arr){
+		x = 22;
+		str = "I like food";
+		arr [0] = 19;
 	}
+	
 
 }
